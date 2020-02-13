1553a5055286a970e23d187d5fe4054c453ac48b
Kadhirash Sivakumar <kaddy@Kadhirashs-MacBook-Pro.local>
2020-02-13 09:43:35 -0800

    Fixed Scrapy spider and able to download necessary link as a .txt file to use later for EDA.

--- square/square/spiders/climatebot.py
+++ square/square/spiders/climatebot.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+#from scrapy import Selector
 
 class ClimatebotSpider(scrapy.Spider):
     name = 'climatebot'
     #allowed_domains = ['http://berkeleyearth.org/data/']
     start_urls = ['http://berkeleyearth.org/data/']
 
+    
     def parse(self, response):
-        #pass
-        print(response.url)
-        # Get all the <a> tags
-        a_selectors = response.xpath("//a")
-        # Loop on each tag
-        for selector in a_selectors:
-            # Extract the link text
-            text = selector.xpath("text()").extract_first()
-            # Extract the link href
-            link = selector.xpath("@href").extract_first()
-            # Create a new Request object
-            request = response.follow(link, callback=self.parse)
-            # Return it thanks to a generator
-            yield request
+        #print(response.url) // Just to make sure spider is scraping and crawling correct website
+        for href in response.xpath('//a[re:test(@href, "Raw_TAVG_complete")]//@href'): # Looking for Raw data for monthly average temperature data 
+            url = response.urljoin(href.extract()) #
+            yield scrapy.Request(url, callback = self.parse_dir_contents)   # create a request to follow the link from the page
+    
+    def parse_dir_contents(self,response):
+        path = response.url.split('/')[-1]
+        self.logger.info('Saving file %s', path)
+        with open(path, 'wb') as f:
+            f.write(response.body) # Download the link into folder for easy access
+
+    
